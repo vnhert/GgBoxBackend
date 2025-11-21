978b7bc6bc39016d385f55f9813d3d6b1b1e1c72
Jimena <ji.galicia@duocuc.cl>
2025-11-21 11:15:23 -0300

    Creacion de backend en springboot...

---
+++ src/main/java/com/example/demo/GgBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgBoxApplication.class, args);
+	}
+
+}
